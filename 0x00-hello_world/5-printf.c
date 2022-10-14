7c0aaab663b0b76668d33143e2b1fc462220c13d
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-13 21:18:04 -0700

    Hello World 02

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ *Main - entry point or main function
+ * return - returns 0, if succesful
+ */
+
+int main() {
+
+  printf("Programming is like building a multilingual puzzle\n");
+    return 0;
+}
