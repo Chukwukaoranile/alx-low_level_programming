83e76618603893c1c700512e3f2280bce9201358
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-14 21:39:34 -0700

    Shell I/O Redirection 03

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+/**
+* Main - prints the alphabet in lowercase, and then in uppercase
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+  char ch;
+  char c;
+
+  for (ch = 'a'; ch <= 'z'; ch++)
+      putchar(ch);
+  for (c = 'A'; c <= 'Z'; c++)
+      putchar('\n');
+    return (0);
+}
