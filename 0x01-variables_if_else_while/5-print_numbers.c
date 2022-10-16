6a6cf3a28b020518eee8aac968cf998362a8738e
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-16 11:18:19 -0700

    alx-low_level_programming 01

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,10 @@
 
 int main(void)
 {
-	char ch;   
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'e' && ch != 'q')
-		putchar(ch);
-	}
-	putchar('\n');
+	int n;   
+
+	for (n = '0'; n <= '9'; n++)
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
