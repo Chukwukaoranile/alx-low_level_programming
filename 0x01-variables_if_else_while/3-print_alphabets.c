3ce4dabe0d6bb815e1784070e53e74fe46ef95a1
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-16 12:17:13 -0700

    alx-low_level_programming 01

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
 * Main - prints the alphabet in lowercase, and then in uppercase
 *
-*Return: Always 0
+* Return: Always 0
 */
 
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 		putchar(ch);
 	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
-	
+
 	putchar('\n');
 	return (0);
 }
