963f6904e738e641974d2d1ed1359424eeccf047
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-16 13:12:12 -0700

    alx-low_level_programming 0001

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,12 @@
 
 /**
 *
-* Main - prints "with proper grammar, but for a piece of art,"
-* return - Always 0,
+* main - prints "with proper grammar, but for a piece of art,"
+* return: Always 0,
 */
 
 int main(void)
 {
-
-printf("Programming is like building a multilingual puzzle\n");
-return (0);
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
