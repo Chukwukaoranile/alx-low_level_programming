9032c39de02f68aa939e298171de019ce05c57b4
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-16 10:27:13 -0700

    alx-low_level_programming 04

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  *main - prints all single digit numbers of base 10 starting from 0
  *
@@ -5,4 +7,14 @@
  */
 
 int main(void)
-  
+{
+	char ch;   
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
