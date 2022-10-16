67b41dd69b005231adc77677472518d7b38d8bff
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-16 15:42:10 -0700

    alx-low_level_programming 01

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - prints with proper grammar, but for a piece of art,
+* main - Prints with proper grammar, but for a piece of art,
 *
-* return: Always 0
+* Return: Always 0
 */
 
 int main(void)
 {
-	printf("%d\n", "with proper grammar, but the outcome is a piece of art,""
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,""
 );
 	return (0);
 }
