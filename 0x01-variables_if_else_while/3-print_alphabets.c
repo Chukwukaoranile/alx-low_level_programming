b8d4df9b506b9ff46f3a2d74d883dce948aa1c6f
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-16 12:22:05 -0700

    alx-low_level_programming 01

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* Main - prints the alphabet in lowercase, and then in uppercase
+* Main - This program will assign a random number to the variable n 
 *
 * Return: Always 0
 */
