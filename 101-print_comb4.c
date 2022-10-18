9aee825224717511c95400154bc94a4e21449ad5
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-17 21:59:51 -0700

    alx-low_level_programming 01

---
+++ 101-print_comb4.c
@@ -0,0 +1,15 @@
+#include
+/**
+ * main - Prints the combination of three numbers
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int num1; num;, num3;
+
+	for (num1 = '0'; num1 <= '3'; num1++)
+	{
+	for (num2 = '0'; num2<= '3'; num2++)                                                                                                  
+	{                     
