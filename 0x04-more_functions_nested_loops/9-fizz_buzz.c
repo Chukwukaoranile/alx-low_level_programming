ba79e1fa155ffc517b26bd06388063ef8d3a4882
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-21 04:04:01 -0700

    alx-low_level_programming 01

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Filter out the 99.5% of programming job candidates.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FIZZBUZZ");
+
+		else if ((num % 3) == 0)
+			printf("FIZZ");
+		else if ((num % 5) == 0)
+			printf("BUZZ");
+		else
+			printf("%d", num);
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
