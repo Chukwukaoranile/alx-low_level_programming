f5d8d0603721234b4387e8d61927ee9bb02baf92
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-21 02:10:51 -0700

    alx-low_level_programming 01

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * print_most_numbers - print the most numbers from 0-9 except 2 and 4.
  */
 
-
 void print_most_numbers(void)
 {
 	int num;
 
-	for (num != 2 && num != 4)
+	for (num = 0; num <= 9; num++)
 	{
+		if (num != 2 && != 4)
 		_putchar((num % 10) + '0');
 	}
 
