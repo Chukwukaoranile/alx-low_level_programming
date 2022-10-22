4bfaa97f7e86c42886ac80c43f01b050606d61f1
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-22 14:15:12 -0700

    alx-low_level_programming 01

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0
+ */
+
+int _putchar(char c)
+{
+	
+	_Putchar("_");
+	_Putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+These projects will demonstrate how nested loops and functions works
