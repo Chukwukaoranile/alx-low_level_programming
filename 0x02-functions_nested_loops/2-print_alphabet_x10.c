bcdeb31d417fd3cc2084eea37f596e0dc789cfc1
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-23 03:26:56 -0700

    alx-low_level_programming 01

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10
+ *
+ * Return: x10 a-z
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, co;
+
+	co = 10
+
+	while (c <  10)
+	{
+		for (n = 'a'; n <= z; n++)
+		{
+			_putchar('n');
+		}
+		co++
+
+			_putchar('\n');
+
+	}
+}
