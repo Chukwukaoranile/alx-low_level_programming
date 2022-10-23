99159b9aa6002f356e0e3373ae172b960cfb29f1
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-23 06:24:23 -0700

    alx-low_level_programming 01

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Success
+ * print_diagonal - Draw a diagonal line using the \ character
+ * @n: The number of \ characters to be printed.
  */
+
 void print_diagonal(int n)
 {
 	int draw, diag;
+
 
 	if (n > 0)
 	{
@@ -19,7 +20,6 @@ void print_diagonal(int n)
 
 			if (draw == n - 1)
 				continue;
-	
 			_putchar('\n');
 		}
 	}
