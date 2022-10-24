1ff602e8d8137e001232a2e0ad43882fe54d8a0e
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-24 15:23:09 -0700

    alx-low_level_programming 01

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s:  variable length of string.
+ * Return: Always 0 (Successful)
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
+
