17c8c7ddd567fce720ccee83b1b2646b7c4307de
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-24 13:15:34 -0700

    alx-low_level_programming 01

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Function that takes a pinter as int
+ *
+ * Return:
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#This codes will demontrsde the basic functions of C pointers, String and Arrays
