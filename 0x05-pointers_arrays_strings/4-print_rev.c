b95d2be65400630cb21eda25fe17e993c4bb21a6
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-25 10:13:28 -0700

    alx-low_level_programming 01

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,9 +3,18 @@
 /**
  * print_rev -  function that prints a string, in reverse.
  * @s: Reverse
+ * _putchar: tpbe printed
  * Return: Always 0 (Reverse string)
  */
 
 void print_rev(char *s)
 {
+	int i = 0;
 
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
