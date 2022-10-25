fc47a7886b032d8f7b416fb1822b325df65468ca
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-25 03:45:15 -0700

    alx-low_level_programming 01

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Function that checks for alphabetic character.
+ * @c: single letter input
+ * Return: 1 if c is a letter
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')
+		return (1);
+	else
+		return (0);
+}
