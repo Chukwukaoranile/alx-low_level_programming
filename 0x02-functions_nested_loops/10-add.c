c52739cf3c5bba48113196482b0547925386ce3a
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-25 04:21:43 -0700

    alx-low_level_programming 01

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -  a function that adds two integers and returns the result.
+ * @one: First number input
+ * @two: Second number input
+ * Return: one + two
+ */
+
+int add(int one, int two)
+{
+	return (one + two);
+}
