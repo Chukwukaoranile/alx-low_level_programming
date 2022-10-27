e563e84b535061a051289a9afb26cea4abe0d7b1
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-27 05:59:58 -0700

    alx-low_level_programming 01

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * strcat - Concatenates the string pointed to by @src,
+ * including the terminating
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * Return: A pointer to the destination string @dest.
  */
 
-void _putchar(char c)
+char *strcat(char *dest, const char *src)
 {
-	return (write(1, &c, 1));
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
