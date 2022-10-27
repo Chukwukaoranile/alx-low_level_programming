f109f49ff7382a4083575ce29e36d9640e5d1505
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-27 11:48:33 -0700

    alx-low_level_programming 01

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,17 +13,17 @@ char *leet(char *str)
 	int lc[5] = {97, 101, 111, 116, 108};
 	int code[5] = {4, 3, 0, 7, 1};
 
-	while (*(s + x))
+	while (*(str + x))
 	{
 		for (y = 0; y < 5; y++)
 		{
 			uc = lc[y] - 32;
-			if (s[x] == lc[y] || s[x] == uc)
+			if (str[x] == lc[y] || str[x] == uc)
 			{
-				s[x] = code[y] + 48;
+				str[x] = code[y] + 48;
 			}
 		}
 		x++;
 	}
-	return (s);
+	return (str);
 }
