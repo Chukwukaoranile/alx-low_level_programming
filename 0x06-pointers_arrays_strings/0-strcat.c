79770b8316366fdc788af702b9cef3f23bd5dbda
Achebe Chika Luke <prosperbinary@gmail.com>
2022-10-27 05:44:18 -0700

    alx-low_level_programming 01

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,7 +8,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c)
+void _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
