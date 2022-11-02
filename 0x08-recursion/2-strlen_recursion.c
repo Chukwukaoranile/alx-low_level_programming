b449745997710a50847ea59fcae4adebb7eead84
Achebe Chika Luke <prosperbinary@gmail.com>
2022-11-02 06:48:19 -0700

    alx-low_level_programming 01

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strlen_recursion - a function that returns the length of a string
+ *
+ * @s: Variable for the length
+ *
+ *  Return: The length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 1;
+
+	if (*s != '\0')
+	{
+		s++;
+		n = n + _strlen_recursion(s);
+
+		return (n);
+
+	}
+	return (0);
+
+}
