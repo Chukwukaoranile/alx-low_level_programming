0173637f8fa1f047bbcbce6e1f822b1a35d17aea
Achebe Chika Luke <prosperbinary@gmail.com>
2022-11-06 20:19:12 -0800

    0x0A-argc_argv Tasks

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This directory contains files that demonstrates static library
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - print the characters
+ * @c: Charater to be printed
+ * Return: successful 0
+ */
+
+int _putchar(char c)
+{
+	return (0);
+}
