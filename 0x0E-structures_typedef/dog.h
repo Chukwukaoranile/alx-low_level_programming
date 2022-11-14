f1a2ea05b52ef1d33f02868771816c5ee48dca02
Achebe Chika Luke <prosperbinary@gmail.com>
2022-11-14 15:53:50 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,29 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-/**
- * struct dog - Define a new type struct dog with the following elements.
- * @name: variable 1
- * @age: variable 2
- * @owner: Parameter
- *
- * Return: successful
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+#ifndef DOG_H
+#define DOG_H
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
-
