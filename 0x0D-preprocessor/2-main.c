e7eb6ed091820c82e5b56dab4ec90f8013bdbaa7
Achebe Chika Luke <prosperbinary@gmail.com>
2022-11-14 14:46:11 -0800

    preprocessor Tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a function-like macro ABS(x) that computes
+ * the absolute value of a number x.
+ *
+ * Return: succesful.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
