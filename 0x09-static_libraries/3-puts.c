78eb2fc68fe47f772a3ac273435ee2670c0081e5
Achebe Chika Luke <prosperbinary@gmail.com>
2022-11-17 20:14:34 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string, followed by a new line, to stdout.
+ * @str: to be printed
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
- * _putchar - print the characters
- * @c: Charater to be printed
- * Return: successful 0
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
  */
 
-int _putchar(char c)
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
