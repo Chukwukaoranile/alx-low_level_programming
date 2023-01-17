0f3730324e8d56b378d5e71c27b39f75c2d89a67
Achebe Chika Luke <prosperbinary@gmail.com>
2023-01-17 01:34:19 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/o-main.c
@@ -0,0 +1,14 @@
+#include "./100-operations.so"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always Exit_SUCCESS.
+ */
+
+int main(void)
+{
+	printf("%d\n", _strlen("Holberton"));
+	return (Exit_SUCCESS);
+}
